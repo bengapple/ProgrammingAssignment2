ce222cc985172c364945fc1acfa022acf41888e1
bengapple <bengapple@gmail.com>
2014-07-23 21:50:34 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -11,3 +11,17 @@ makeCacheMatrix <- function(x = matrix(),...) {
              setsolve = setsolve,
              getsolve = getsolve)
 }
+
+
+
+cacheSolve <- function(x, ...) {
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+}
